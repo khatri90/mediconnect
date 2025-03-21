042f06a47a84806982ff4c3c6df55193e18cf3b0
khatri90 <152983741+khatri90@users.noreply.github.com>
2025-03-21 19:52:13 +0500

    Update urls.py

--- doctors/urls.py
+++ doctors/urls.py
@@ -3,11 +3,11 @@ from .views import DoctorRegistrationAPIView, DoctorRegistrationStatusAPIView, D
 
 
 urlpatterns = [
-    path('api/doctors/register/', DoctorRegistrationAPIView.as_view(), name='doctor-register'),
-    path('api/doctors/status/<int:doctor_id>/', DoctorRegistrationStatusAPIView.as_view(), name='doctor-status'),
-    path('api/doctors/login/', DoctorLoginAPIView.as_view(), name='doctor-login'),
-    path('api/doctors/change-password/', ChangePasswordAPIView.as_view(), name='doctor-change-password'),
-    path('api/doctors/profile/', DoctorProfileAPIView.as_view(), name='doctor-profile'),
+    path('doctors/register/', DoctorRegistrationAPIView.as_view(), name='doctor-register'),
+    path('doctors/status/<int:doctor_id>/', DoctorRegistrationStatusAPIView.as_view(), name='doctor-status'),
+    path('doctors/login/', DoctorLoginAPIView.as_view(), name='doctor-login'),
+    path('doctors/change-password/', ChangePasswordAPIView.as_view(), name='doctor-change-password'),
+    path('doctors/profile/', DoctorProfileAPIView.as_view(), name='doctor-profile'),
 
 ]
 
